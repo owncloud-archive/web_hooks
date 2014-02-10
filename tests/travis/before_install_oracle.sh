4d0d06a8763f66aba36f188636715c34a2ff91ee
Thomas Müller <thomas.mueller@tmit.eu>
2014-02-10 21:38:15 +0100

    Fix before_install_oracle.sh

--- tests/travis/before_install_oracle.sh
+++ tests/travis/before_install_oracle.sh
@@ -109,7 +109,7 @@ echo "ALTER SYSTEM SET PROCESSES=40 SCOPE=SPFILE;" | \
 sqlplus -S -L sys/admin AS SYSDBA
 
 # Set Oracle environment variables on login.
-cat <<END >>/root/.bashrc
+cat <<END >>~/.bashrc
 
 . /usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/oracle_env.sh
 END
